6a87564eb55efba413c65ca0bb73db4872f6a449
mdronski <dronskimichal@gmail.com>
2020-03-07 00:05:01 +0100

    cleanup code

--- src/preprocessing/annotations_conversion.py
+++ src/preprocessing/annotations_conversion.py
@@ -40,30 +40,30 @@ class AnnotationsConverter:
         self.__save_mask_image(base_output_path, mask)
         self.__save_masked_rgba_image(base_output_path, img_RGBA, mask)
 
-    def __save_masked_rgba_image(self, base_output_path, img_RGBA, mask):
-        img_RGBA[mask, :4] = 255
-        masked_image_path = base_output_path + '_masked_image.png'
-        cv.imwrite(masked_image_path, img_RGBA)
+    def __load_rgba_image(self, img: np.ndarray) -> np.ndarray:
+        r_channel, g_channel, b_channel = cv.split(img)
+        alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 255  # creating a dummy alpha channel image.
+        img_RGBA = cv.merge((r_channel, g_channel, b_channel, alpha_channel))
+        return img_RGBA
 
-    def __save_mask_image(self, base_output_path, mask):
-        mask_path = base_output_path + '_mask.png'
-        cv.imwrite(mask_path, mask * 255)
-
-    def __create_mask(self, base_output_path):
-        annotation_path = base_output_path + '.json'
-        annotation = AnnotationParser.parse_annotation(annotation_path)
-        mask = create_mask_image([annotation.shapes['adaptator']], annotation.image_size)
-        return mask
-
-    def __create_base_output_path(self, image_path):
+    def __create_base_output_path(self, image_path: str) -> str:
         image_name_without_extension = os.path.basename(image_path).split(sep=".")[0]
         curr_dir_path = os.path.dirname(image_path)
         output_path = os.path.join(curr_dir_path, self.__output_sub_directories_name)
         base_output_path = os.path.join(output_path, image_name_without_extension)
         return base_output_path
 
-    def __load_rgba_image(self, img):
-        r_channel, g_channel, b_channel = cv.split(img)
-        alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 255  # creating a dummy alpha channel image.
-        img_RGBA = cv.merge((r_channel, g_channel, b_channel, alpha_channel))
-        return img_RGBA
+    def __create_mask(self, base_output_path: str) -> np.ndarray:
+        annotation_path = base_output_path + '.json'
+        annotation = AnnotationParser.parse_annotation(annotation_path)
+        mask = create_mask_image([annotation.shapes['adaptator']], annotation.image_size)
+        return mask
+
+    def __save_mask_image(self, base_output_path: str, mask: np.ndarray) -> None:
+        mask_path = base_output_path + '_mask.png'
+        cv.imwrite(mask_path, mask * 255)
+
+    def __save_masked_rgba_image(self, base_output_path: str, img_rgba: np.ndarray, mask: np.ndarray) -> None:
+        img_rgba[mask, :4] = 255
+        masked_image_path = base_output_path + '_masked_image.png'
+        cv.imwrite(masked_image_path, img_rgba)
