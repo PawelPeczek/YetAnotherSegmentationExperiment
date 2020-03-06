b9fbe183791d5f18504dcdb1f47a6d9c3937bd34
mdronski <dronskimichal@gmail.com>
2020-03-06 22:15:01 +0100

    change polylines to fillpoly

--- src/utils/images.py
+++ src/utils/images.py
@@ -13,12 +13,10 @@ def create_mask_image(shapes: List[Shape], size: ImageSize) -> BinaryMask:
         for shape in shapes
     ]
     points = np.array(points, np.int32)
-    points = points.reshape((-1, 1, 2))
-    cv.polylines(
+    points = points.squeeze(axis=0)
+    cv.fillPoly(
         img=mask,
-        pts=points,
-        isClosed=True,
+        pts=[points],
         color=255,
-        thickness=-1
     )
     return mask.astype(np.bool)
