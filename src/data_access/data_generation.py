a156e5b5ad20e02d167b6107775a21c132d57839
mdronski <dronskimichal@gmail.com>
2020-04-06 23:58:48 +0200

    support for multiple class detailed dataset generation

--- src/data_access/data_generation.py
+++ src/data_access/data_generation.py
@@ -5,7 +5,7 @@ import numpy as np
 from tensorflow.python.keras.utils import Sequence
 
 from src.data_access.data_transformations import DataTransformationChain
-from src.primitives.data_access import DataSetExampleDescription
+from src.primitives.data_access import DataSetExampleDescription, DataSetExampleBatch
 
 
 class DataGenerator(Sequence):
@@ -31,3 +31,39 @@ class DataGenerator(Sequence):
         return self.__transformation_chain.transform_batch(
             example_descriptions=batch_examples
         )
+
+
+class DetailedDataGenerator(Sequence):
+    def __init__(self,
+                 data: List[Tuple[List[DataSetExampleDescription], DataTransformationChain]],
+                 batch_size: int = 32):
+        super().__init__()
+        self.__data = data
+        self.__batch_size = batch_size
+
+    def on_epoch_end(self) -> None:
+        self.__examples = self.__prepare_examples()
+        e, t = self.__examples
+        l = list(zip(e, t))
+        random.shuffle(l)
+        self.__examples = l
+
+    def __len__(self) -> int:
+        return int(np.floor(len(self.__examples) / self.__batch_size))
+
+    def __getitem__(self, index: int) -> Tuple[np.ndarray, np.ndarray]:
+        start_index, end_index = index * self.__batch_size, (index + 1) * self.__batch_size
+        return self.__examples[start_index:end_index]
+
+    def __prepare_examples(self) -> DataSetExampleBatch:
+        examples_truths = []
+        for image_paths, transformation_chain in self.__data:
+            examples_truths.append(transformation_chain.transform_batch(image_paths))
+
+        all_batches = examples_truths[0][0]
+        all_truths = examples_truths[0][1]
+        for example_batch, ground_truths in examples_truths[1:]:
+            all_batches = np.concatenate((all_batches, example_batch))
+            all_truths = np.concatenate((all_truths, ground_truths))
+
+        return all_batches, all_truths
--- src/data_access/detailed_dataset_generation.py
+++ src/data_access/detailed_dataset_generation.py
@@ -1,41 +1,44 @@
 import os
 from glob import glob
+from typing import List, Tuple
 
+import cv2 as cv
+
+from src.config import DATASET_PATH, BACKGROUNDS_DIR_NAME, MODEL_INPUT_SIZE
 from src.data_access.config import TRAINING_ANGLES, TEST_ANGLES, AUGMENTATIONS
-from src.data_access.folds_generation import FoldsGenerator
-from src.primitives.data_access import RandomSplitSpecs, RotationBasedClassSplit, FoldsGeneratorSpecs
-from src.config import DATASET_PATH, BACKGROUNDS_DIR_NAME, CLASS_MAPPINGS, MODEL_INPUT_SIZE
 from src.data_access.data_transformations import EntryTransformation, DataTransformationChain, DataStandardisation, \
     DataAugmentation
+from src.data_access.folds_generation import FoldsGenerator
+from src.primitives.data_access import RandomSplitSpecs, RotationBasedClassSplit, FoldsGeneratorSpecs, \
+    DataSetExampleDescription
 from src.utils.images import load_image
-import cv2 as cv
 
 
 class DetailedDatasetHandler:
     def __init__(self,
-                 clazz: str,
+                 classes: List[str],
                  difficulty: str) -> None:
-        self._clazz = clazz
+        self._classes = classes
         self._difficulty = difficulty
 
-    def generate_backgrounds_wildcard(self):
+    def __generate_backgrounds_wildcard(self, clazz: str) -> str:
         class_background_path_wildcard = {
             "all": "*/",
             "easy": "easy/",
             "hard": "blending/"
         }[self._difficulty]
-        return os.path.join(DATASET_PATH, BACKGROUNDS_DIR_NAME, self._clazz, class_background_path_wildcard, "*.jpg")
+        return os.path.join(DATASET_PATH, BACKGROUNDS_DIR_NAME, clazz, class_background_path_wildcard, "*.jpg")
 
-    def generate_paths(self):
-        wildcard = self.generate_backgrounds_wildcard()
+    def __generate_paths(self, clazz: str):
+        wildcard = self.__generate_backgrounds_wildcard(clazz)
         return glob(wildcard)
 
-    def generate_transformation_chain(self, standarize: bool = False, augument: bool = False):
-        backgrounds_paths = self.generate_paths()
+    def __generate_transformation_chain(self, clazz: str, standarize: bool = False, augument: bool = False) -> DataTransformationChain:
+        backgrounds_paths = self.__generate_paths(clazz)
         backgrounds = [load_image(path, cv.COLOR_BGR2RGB) for path in backgrounds_paths]
 
         entry_transformation = EntryTransformation(
-            class_mapping={self._clazz: 1},
+            class_mapping={clazz: 1},
             target_size=MODEL_INPUT_SIZE,
             backgrounds=backgrounds
         )
@@ -57,25 +60,39 @@ class DetailedDatasetHandler:
             augmentations=augumentations
         )
 
-    def generate_folds_generator(self,):
+    def generate_folds_generator(self, clazz: str) -> FoldsGenerator:
         random_split_specs = RandomSplitSpecs(
-            classes={self._clazz},
+            classes={clazz},
             splits_number=1,
             training_samples_factor=0.8
         )
 
         rotation_based_class_split = RotationBasedClassSplit(
-            training_angles={self._clazz: TRAINING_ANGLES[self._clazz]},
-            test_angles={self._clazz: TEST_ANGLES[self._clazz]}
+            training_angles={clazz: TRAINING_ANGLES[clazz]},
+            test_angles={clazz: TEST_ANGLES[clazz]}
         )
 
         folds_generator_specs = FoldsGeneratorSpecs(
             random_split_specs=random_split_specs,
             rotation_based_splits=[rotation_based_class_split],
-            allowed_background_wildcard=self.generate_backgrounds_wildcard()
+            allowed_background_wildcard=self.__generate_backgrounds_wildcard(clazz)
         )
 
         return FoldsGenerator(
             dataset_path=DATASET_PATH,
             generator_specs=folds_generator_specs
         )
+
+    def prepare_generators_and_transformations(self) -> List[Tuple[FoldsGenerator, DataTransformationChain]]:
+        return [
+            (self.generate_folds_generator(clazz), self.__generate_transformation_chain(clazz))
+            for clazz in self._classes
+        ]
+
+    def prepare_folds_and_transformations(self, standarize: bool = False, augument: bool = False) -> List[Tuple[List[DataSetExampleDescription], DataTransformationChain]]:
+        return [
+            (next(self.generate_folds_generator(clazz).generate_folds()).training_set.examples,
+             self.__generate_transformation_chain(clazz, standarize, augument))
+            for clazz in self._classes
+        ]
+
